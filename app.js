da2e736ff72e1e547fc8b41c4c136f655bc70192
Md Ariful Islam Palash <Md Ariful Islam Palash>
2025-03-22 14:39:17 +0600

    Basic setup in app.js, .env and package.js for start the server

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+dotenv.config();
+
+//Database connection
+const cookieParser = require("cookie-parser");
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connection Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Request parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // For html encoded
+
+//set view engine
+app.set("view engine", "ejs");
+
+//set static folder
+app.use(express.static(path.join(__dirname)));
+
+//parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//ROUTING SETUP
+
+//Error handling
+
+app.listen(process.env.PORT, () => {
+  console.log(`The application listening to Port ${process.env.PORT}`);
+});
