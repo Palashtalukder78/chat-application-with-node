3985cb111ae11ba80377174f02852f3227db36d1
Md Ariful Islam Palash <Md Ariful Islam Palash>
2025-03-23 13:40:39 +0600

    for form handle (1)file upload->make a middleware(AvatarUpload), in utilities(singleUploader)

---
+++ middlewares/users/avatarUpload.js
@@ -0,0 +1,25 @@
+const uploader = require("../../utilities/singleUploader");
+
+function avatarUpload(req, res, next) {
+  const upload = uploader(
+    "avatars",
+    ["image/jpg", "image/jpeg", "image/png"],
+    1000000,
+    "only jpg,jpeg and png allowed"
+  );
+  //call the middleware function
+  upload.any()(req, res, (err) => {
+    if (err) {
+      res.status(500).json({
+        errors: {
+          avatar: {
+            msg: err.message,
+          },
+        },
+      });
+    } else {
+      next();
+    }
+  });
+}
+module.exports = avatarUpload;
